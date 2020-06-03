2cb822eac0d67de1a91bc555a19616cca23cc304
thakyZ <thakyz@outlook.com>
2020-06-03 13:21:16 -0700

    Testing if this update works.

---
+++ 1.1/Source/PsychologyImprisonmentPatch/Psychpatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Psychology;
+using RimWorld;
+using System;
+using Verse;
+
+namespace PsychologyImprisonmentPatch
+{
+  public class PsychologyPatcher
+  {
+    public static void DoPatching()
+    {
+      var harmony = new Harmony("com.dninemfive.psychologyimprisonmentpatch");
+      harmony.PatchAll();
+      Log.Message("Psychology Imprisonment Patch successfully loaded");
+    }
+  }
+
+  [HarmonyPatch(typeof(InteractionWorker_RecruitAttempt), "DoRecruit")]
+  [HarmonyPatch(new Type[] { typeof(Pawn), typeof(Pawn), typeof(float), typeof(string), typeof(string), typeof(bool), typeof(bool) }, new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Normal })]
+  class RecruitAttemptPatch
+  {
+    public static bool RemoveCapturedThoughts(Pawn recruiter, Pawn recruitee)
+    {
+      if(recruitee.IsPrisonerOfColony && recruitee.Faction != recruiter.Faction)
+      {
+          recruitee.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOfPsychology.CapturedMe);
+      }
+      return true;
+    }
+  }
+}
